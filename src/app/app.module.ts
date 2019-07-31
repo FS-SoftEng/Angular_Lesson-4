9a756f10708c408f4ae4ce3ea6f2b4dec12d9a61
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-30 22:44:44 -0500

    Angular Lesson 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+
+// add the following import statement:
+
+import { SkillsService } from './services/skills.service';
+
+@NgModule({
+  declarations: [AppComponent, SkillsComponent],
+  imports: [BrowserModule],
+  
+  // add SkillsService to the providers array:
+  providers: [SkillsService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
